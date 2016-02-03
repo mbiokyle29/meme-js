ac9b42e521d41104e89ed456198e95e4a5936370
Kyle <mbio.kyle@gmail.com>
2016-02-02 22:52:57 -0600

    inital commit

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+var webpack = require('webpack');
+
+module.exports = {
+
+    entry: __dirname+"/src/meme.js",
+
+    output: {
+        path: __dirname +"/dist/",
+        filename: "meme.js",
+    }
+};
