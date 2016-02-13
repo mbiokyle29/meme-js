539e4fb4280091327baca68e77e6019289353d86
Kyle <mbio.kyle@gmail.com>
2016-02-13 17:15:20 -0600

    src requirements using path for windows compat - do it right with __dirname

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
 var path = require('path');
-Meme = require(path.join('.',' meme.js'));
-require(path.join('.', "style.css"));
+Meme = require(path.join(__dirname ,' meme.js'));
+require(path.join(__dirname, "style.css"));
 require("font-awesome-webpack");
